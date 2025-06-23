1803712477f9327d8556487739d954807bad0e13
Joaoganha <joaolucastina851@gmail.com>
2025-06-23 17:31:16 -0300

    jogo da velha

---
+++ jogo da velha/script.js
@@ -0,0 +1,33 @@
+var jogador = "x";
+
+function jogar(celula){
+
+    //compara se a célula está vazia para assim escrever
+    if(celula.innerHTML == ""){
+
+        //escreva a letra na célula
+        celula.innerHTML = jogador;
+
+        //alterna a variável jogador para a próxima jogada
+        if(jogador == "x"){
+            jogador = "o";
+            celula.style.backgroundColor = "yellow";
+        }else{
+                jogador = "x"
+                celula.style.backgroundColor = "green";
+            
+            }
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
